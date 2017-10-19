590192284fd171eaeb7d67d36c41e6d108306df4
Dennis Siemensma <github@dennissiemensma.nl>
2017-10-19 21:04:32 +0200

    Source data retention (DSMR-readings) #72

--- dsmr_datalogger/admin.py
+++ dsmr_datalogger/admin.py
@@ -3,14 +3,19 @@ from django.contrib import admin
 from solo.admin import SingletonModelAdmin
 
 from dsmr_backend.mixins import ReadOnlyAdminModel
-from .models.settings import DataloggerSettings
+from .models.settings import DataloggerSettings, RetentionSettings
 from .models.reading import DsmrReading
-from dsmr_datalogger.models.statistics import MeterStatistics
+from .models.statistics import MeterStatistics
 
 
 @admin.register(DataloggerSettings)
 class DataloggerSettingsAdmin(SingletonModelAdmin):
     list_display = ('com_port', )
+
+
+@admin.register(RetentionSettings)
+class RetentionSettingsAdmin(SingletonModelAdmin):
+    list_display = ('data_retention_in_hours', )
 
 
 @admin.register(DsmrReading)
--- dsmr_datalogger/models/settings.py
+++ dsmr_datalogger/models/settings.py
@@ -55,3 +55,41 @@ class DataloggerSettings(SingletonModel):
     class Meta:
         default_permissions = tuple()
         verbose_name = _('Datalogger configuration')
+
+
+class RetentionSettings(SingletonModel):
+    RETENTION_NONE = None
+    RETENTION_DAY = 24
+    RETENTION_WEEK = 7 * RETENTION_DAY
+    RETENTION_MONTH = 4 * RETENTION_WEEK
+    RETENTION_HALF_YEAR = 6 * RETENTION_MONTH
+    RETENTION_YEAR = 12 * RETENTION_MONTH
+    RETENTION_CHOICES = (
+        (RETENTION_NONE, _('None (keep all readings)')),
+        (RETENTION_DAY, _('Discard most readings after 24 hours')),
+        (RETENTION_WEEK, _('Discard most readings after one week')),
+        (RETENTION_MONTH, _('Discard most readings after one month')),
+        (RETENTION_HALF_YEAR, _('Discard most readings after six months')),
+        (RETENTION_YEAR, _('Discard most readings after one year')),
+    )
+
+    data_retention_in_hours = models.IntegerField(
+        blank=True,
+        null=True,
+        default=RETENTION_NONE,
+        choices=RETENTION_CHOICES,
+        verbose_name=_('Data retention'),
+        help_text=_(
+            'By default the application stores all readings taken. As there might be DSMR-readings about every second, '
+            'this results in several millions of readings each year. This may cause degraded performance. '
+            'For that reason you might want to apply retention to this data. Please note that enabling this feature '
+            'will NOT discard ALL readings, as it will PRESERVE the first reading of each hour.'
+        )
+    )
+
+    def __str__(self):
+        return self._meta.verbose_name.title()
+
+    class Meta:
+        default_permissions = tuple()
+        verbose_name = _('Retention configuration')
